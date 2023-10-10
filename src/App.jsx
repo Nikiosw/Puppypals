efca2edfb4cacf5004689e596979817febfb53e0
Nikios Wallace <nikiosmwallace@gmail.com>
2023-10-10 17:04:21 -0400

    final code cleaned up

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,7 @@ function App() {
   
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      
       {
           puppies.map((puppy) => {
             return <p onClick={ () => handleClick2(puppy) } key = {puppy.id}>{puppy.name}</p>
@@ -41,10 +32,7 @@ function App() {
           </ul>
         </div>
       )}
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
